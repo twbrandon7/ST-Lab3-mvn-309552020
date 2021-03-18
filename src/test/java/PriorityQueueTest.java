80ae745e9f81292760b76374acce20b757913a08
Li-Xian Chen <twbrandon7@gmail.com>
2021-03-18 17:13:27 +0800

    add README

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -1,5 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PriorityQueueTest {
-
+    @Test
+    public void someFunc() {
+        System.out.println("Hello");
+    }
 }
